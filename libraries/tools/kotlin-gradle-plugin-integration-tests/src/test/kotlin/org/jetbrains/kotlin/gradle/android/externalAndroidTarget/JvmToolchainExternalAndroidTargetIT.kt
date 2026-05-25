02905d621bcdde09fef908c19cc0ccee2c7e712a
Ademi Abdykerimova <ademi.abdykerimova@jetbrains.com>
2026-05-25 23:48:06 +0200

    Added jvmToolchain tests for extended android target
    Refine jvmToolchain checks for external android target tests
    
    Refactor jvmToolchain tests to use build script returns
    
    Fix withJava test to use correct task
    
    Readability improvements
    
    switch toolchain assertion to JDK 11 to prove toolchain affects jdkHome
    
    assert -jdk-home and clarify JDK path variables
    
    Rewrite to functional tests
    
    update with new dsl and agp version
    
    update with new dsl and agp version

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/android/externalAndroidTarget/JvmToolchainExternalAndroidTargetIT.kt
@@ -0,0 +1,179 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.android.externalAndroidTarget
+
+import org.gradle.api.tasks.compile.JavaCompile
+import org.gradle.util.GradleVersion
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilerArgumentsProducer
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.testbase.*
+import org.jetbrains.kotlin.gradle.uklibs.ignoreAccessViolations
+import kotlin.test.assertEquals
+
+// Used AGP 9.0 as the minimal stable version supported for the android library
+@AndroidTestVersions(minVersion = TestVersions.AGP.AGP_90)
+@AndroidGradlePluginTests
+class JvmToolchainExternalAndroidTargetIT : KGPBaseTest() {
+
+    @GradleAndroidTest
+    fun `test - jvmToolchain configures compiler arguments for androidLibrary and androidHostTest`(
+        gradleVersion: GradleVersion,
+        androidVersion: String,
+        jdkVersion: JdkVersions.ProvidedJdk,
+    ) {
+        externalAndroidLibraryProject(
+            gradleVersion = gradleVersion,
+            androidVersion = androidVersion,
+            jdkVersion = jdkVersion,
+            namespace = "org.jetbrains.sample.multitarget",
+            androidLibraryConfiguration = "withHostTest {}",
+            kotlinConfiguration = "jvmToolchain(11)",
+        ) {
+
+            val taskConfigurations = kotlinTaskConfigurations("compileAndroidMain", "compileAndroidHostTest")
+            val expectedConfiguration = mapOf(
+                "jvmTarget" to "11",
+                "toolchainHome" to jdk11Info.jdkRealPath,
+            )
+
+            assertEquals(expectedConfiguration, taskConfigurations.getValue("compileAndroidMain"))
+            assertEquals(expectedConfiguration, taskConfigurations.getValue("compileAndroidHostTest"))
+        }
+    }
+
+    @GradleAndroidTest
+    fun `test - jvmToolchain configures Kotlin and Java tasks for androidLibrary withJava`(
+        gradleVersion: GradleVersion,
+        androidVersion: String,
+        jdkVersion: JdkVersions.ProvidedJdk,
+    ) {
+        externalAndroidLibraryProject(
+            gradleVersion = gradleVersion,
+            androidVersion = androidVersion,
+            jdkVersion = jdkVersion,
+            namespace = "org.jetbrains.sample.withjava",
+            androidLibraryConfiguration = "withJava()",
+            kotlinConfiguration = "jvmToolchain(11)",
+        ) {
+
+            val kotlinTaskConfiguration = kotlinTaskConfigurations("compileAndroidMain").getValue("compileAndroidMain")
+            val javaTaskConfiguration = javaTaskConfiguration("compileAndroidMainJavaWithJavac")
+
+            assertEquals(
+                mapOf(
+                    "jvmTarget" to "11",
+                    "toolchainHome" to jdk11Info.jdkRealPath,
+                ),
+                kotlinTaskConfiguration,
+            )
+            assertEquals(
+                mapOf("toolchainHome" to jdk11Info.jdkRealPath),
+                javaTaskConfiguration,
+            )
+        }
+    }
+
+    @GradleAndroidTest
+    fun `test - jvmToolchain and jvmTarget configure androidLibrary task independently`(
+        gradleVersion: GradleVersion,
+        androidVersion: String,
+        jdkVersion: JdkVersions.ProvidedJdk,
+    ) {
+        externalAndroidLibraryProject(
+            gradleVersion = gradleVersion,
+            androidVersion = androidVersion,
+            jdkVersion = jdkVersion,
+            namespace = "com.example.lib",
+            androidLibraryConfiguration = "compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)",
+            kotlinConfiguration = "jvmToolchain(17)",
+        ) {
+
+            assertEquals(
+                mapOf(
+                    "jvmTarget" to "11",
+                    "toolchainHome" to jdk17Info.jdkRealPath,
+                ),
+                kotlinTaskConfigurations("compileAndroidMain").getValue("compileAndroidMain"),
+            )
+        }
+    }
+
+    private fun externalAndroidLibraryProject(
+        gradleVersion: GradleVersion,
+        androidVersion: String,
+        jdkVersion: JdkVersions.ProvidedJdk,
+        namespace: String,
+        androidLibraryConfiguration: String = "",
+        kotlinConfiguration: String = "",
+        configureProject: TestProject.() -> Unit = {},
+    ): TestProject = project(
+        "empty",
+        gradleVersion = gradleVersion,
+        buildOptions = defaultBuildOptions.copy(androidVersion = androidVersion),
+        buildJdk = jdkVersion.location,
+    ) {
+        buildGradle.toFile().delete()
+        buildGradleKts.toFile().writeText(
+            """
+            plugins {
+                kotlin("multiplatform")
+                id("com.android.kotlin.multiplatform.library")
+            }
+
+            kotlin {
+                androidLibrary {
+                    compileSdk = 34
+                    namespace = "$namespace"
+            ${androidLibraryConfiguration.trim().prependIndent("        ")}
+                }
+            ${kotlinConfiguration.trim().prependIndent("    ")}
+            }
+            """.trimIndent()
+        )
+        projectPath.resolve("gradle.properties").toFile().appendText(
+            """
+
+            org.gradle.java.installations.auto-download=false
+            org.gradle.java.installations.auto-detect=false
+            org.gradle.java.installations.paths=${jdk11Info.javaHome},${jdk17Info.javaHome},${jdkVersion.location}
+            """.trimIndent()
+        )
+        configureProject()
+    }
+
+    private fun TestProject.kotlinTaskConfigurations(vararg taskNames: String): Map<String, Map<String, String>> =
+        buildScriptReturn {
+            project.ignoreAccessViolations {
+                taskNames.associateWith { taskName ->
+                    val task = project.tasks.named(taskName, KotlinCompile::class.java).get()
+                    val arguments = task.createCompilerArguments(KotlinCompilerArgumentsProducer.CreateCompilerArgumentsContext.default)
+                    val toolchainHome = task.kotlinJavaToolchain.javaVersion
+                        .map { version ->
+                            when (version.majorVersion) {
+                                "11" -> jdk11Info.jdkRealPath
+                                "17" -> jdk17Info.jdkRealPath
+                                else -> error("Unexpected Java toolchain version for '$taskName': ${version.majorVersion}")
+                            }
+                        }
+                        .get()
+                    mapOf(
+                        "jvmTarget" to arguments.jvmTarget.orEmpty(),
+                        "toolchainHome" to toolchainHome,
+                    )
+                }
+            }
+        }.buildAndReturn(":help")
+
+    private fun TestProject.javaTaskConfiguration(taskName: String): Map<String, String> =
+        buildScriptReturn {
+            project.ignoreAccessViolations {
+                val task = project.tasks.named(taskName, JavaCompile::class.java).get()
+                mapOf(
+                    "toolchainHome" to task.javaCompiler.get().metadata.installationPath.asFile.toPath().toRealPath().toString(),
+                )
+            }
+        }.buildAndReturn(":help")
+}
